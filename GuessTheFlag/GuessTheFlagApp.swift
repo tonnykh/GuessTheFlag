658e9ddcbd0543ca36c62cae646f12b46ecb1068
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-23 14:31:52 +0530

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Khumbongmayum Tonny on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
